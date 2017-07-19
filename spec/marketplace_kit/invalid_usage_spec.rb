ed2f44ae03dd2d4ca98af2e592cca37ab769a4cc
Michal Janeczek <michal.janeczek@ymail.com>
2017-07-19 22:55:51 +0200

    Raise error when no .builder file found, refactor config class

--- spec/marketplace_kit/invalid_usage_spec.rb
+++ spec/marketplace_kit/invalid_usage_spec.rb
@@ -1,7 +1,10 @@
 describe "invalid usage" do
-  context "no arguments" do
-    it 'aborts with usage when no arguments passed' do
-      expect{execute_command('') }.to raise_error("Usage: nearme-marketpalce sync | deploy | pull")
-    end
+  it 'aborts with usage when no arguments passed' do
+    expect{execute_command('') }.to raise_error("Usage: nearme-marketpalce sync | deploy | pull")
+  end
+
+  it 'aborts when no .builder file found' do
+    expect(File).to receive(:read).with("#{MarketplaceKit.builder_folder}.builder").and_raise(Errno::ENOENT)
+    expect{execute_command('sync') }.to raise_error("Please create .builder file in order to continue.")
   end
 end
