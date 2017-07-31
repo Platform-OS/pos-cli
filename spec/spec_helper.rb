f27146ae6f1993d1f0c7b1a39a53ab5e5318ef97
Michal Janeczek <michal.janeczek@ymail.com>
2017-07-31 16:07:54 +0200

    Autoload for helpers, spec_helper refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,30 +4,14 @@ SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'marketplace_kit'
-require 'helpers/command_executor'
-require 'helpers/listen_gem_stub'
-require 'helpers/file_mock'
+Dir["#{File.dirname(__FILE__)}/helpers/**/*.rb"].each { |file| require file }
 
 RSpec.configure do |config|
-  config.include Helpers::CommandExecutor
   config.include Helpers::ListenGemStub
   config.include Helpers::FileMock
+  config.include Helpers::CommandExecutor
 
   config.before(:each) do
-    allow_any_instance_of(Object).to receive(:sleep).and_return(nil)
-    allow_any_instance_of(Object).to receive(:system).and_call_original
-    allow_any_instance_of(Object).to receive(:system).with("curl -o marketplace_release.zip 'http://fake-zip-file-url.com'").and_return(nil)
-    allow_any_instance_of(Object).to receive(:system).with("unzip -o marketplace_release.zip -d marketplace_builder").and_return(nil)
-
     @fake_listener = stub_listen_gem
-
-    stub_request(:get, 'http://localhost:3000/api/marketplace_builder/sessions?temporary_token=example-user-token')
-      .to_return(status: 200, body: { login_required: false }.to_json)
-
-    stub_request(:get, 'http://localhost:3000/api/marketplace_builder/sessions?temporary_token=expired-token')
-      .to_return(status: 200, body: { login_required: true }.to_json)
-
-    stub_request(:get, 'http://localhost:3000/api/marketplace_builder/marketplace_releases/1')
-      .to_return(status: 200, body: { id: 1, status: 'success', zip_file: { url: 'http://fake-zip-file-url.com' } }.to_json)
   end
 end
