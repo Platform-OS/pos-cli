5227e93486bb9cc35fefff75be926abd1706c86e
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2017-11-15 13:17:19 +0100

    Don't mess with encoding in normal files

---
+++ spec/marketplace_kit/api_driver_spec.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+describe 'ApiDriver' do
+  subject { MarketplaceKit::Services::ApiDriver.new(:post, 'foo', body, {}).send_request }
+  let(:body) { {
+      path: 'pages/faq.liquid',
+      marketplace_builder_file_body: %q{
+---
+name: "This ’ apostrophe is a problem"
+---
+foo
+}
+               } }
+
+  before(:each) do
+    stub_request(:post, "http://localhost:3000/api/marketplace_builder/foo")
+      .with(body: "{\"path\":\"pages/faq.liquid\",\"marketplace_builder_file_body\":\"\\n---\\nname: \\\"This ’ apostrophe is a problem\\\"\\n---\\nfoo\\n\"}")
+      .to_return(status: 200, body: ''.to_json)
+  end
+
+  it 'dont mess with encoding' do
+    MarketplaceKit.config.load('localhost')
+    expect(subject.success?).to be true
+  end
+end
