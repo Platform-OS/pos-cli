42a936ea6c5abb3cdd45aa2291dfb0d66b31dbe9
Pawel Kowalski <pavelloz@gmail.com>
2018-04-09 23:18:35 +0200

    Add init command

---
+++ marketplace-kit-init.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const program = require('commander'),
+  request = require('request'),
+  fs = require('fs'),
+  path = require('path'),
+  shell = require('shelljs'),
+  extract = require('extract-zip'),
+  logger = require('./lib/kit').logger,
+  version = require('./package.json').version;
+
+const { mkdir, mv, cp, rm, pwd } = shell;
+
+const DEFAULT_URL = 'https://github.com/mdyd-dev/directory-structure/archive/master.zip';
+
+const TMP_DIR = path.resolve(process.cwd(), '.tmp');
+const TMP_PATH = path.resolve(TMP_DIR, 'directory-structure.zip');
+const EXTRACTED_STRUCTURE = path.resolve(TMP_DIR, 'directory-structure-master', 'marketplace_builder');
+
+const emptyTemp = () => rm('-rf', `${TMP_DIR}/*`);
+const createTemp = () => mkdir('-p', TMP_DIR);
+const removeTemp = () => rm('-rf', TMP_DIR);
+
+const downloadZip = (url = DEFAULT_URL) => {
+  logger.Info('Downloading directory structure from ', url);
+  return request(url).pipe(fs.createWriteStream(TMP_PATH));
+};
+
+const extractZip = () => {
+  const moveStructureToDestination = () => mv('-nf', EXTRACTED_STRUCTURE, pwd());
+
+  extract(TMP_PATH, { dir: TMP_DIR }, error => {
+    if (error) {
+      logger.Error('Zip extraction failed. ', error);
+    }
+
+    moveStructureToDestination();
+    removeTemp();
+
+    logger.Success('Directory structure created.');
+  });
+};
+
+const init = () => {
+  createTemp();
+  emptyTemp();
+
+  downloadZip().on('close', extractZip);
+};
+
+program.version(version).parse(process.argv);
+
+init();
