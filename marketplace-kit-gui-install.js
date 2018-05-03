455e7bdd9360f4e15aca395991f92fcd48370c02
Paweł Kowalski <pavelloz@Gmail.com>
2018-05-03 19:34:55 +0200

    Small refactor

--- marketplace-kit-gui-install.js
+++ marketplace-kit-gui-install.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const program = require('commander'),
-  path = require('path'),
   shell = require('shelljs'),
   logger = require('./lib/kit').logger,
   version = require('./package.json').version;
@@ -9,11 +8,11 @@ const program = require('commander'),
 program
   .version(version)
   .arguments('<module-name>', 'module name', 'mp-admin')
-  .option('-s --slug <slug>', 'TODO: install module under slug', 'mp-admin-index')
+  // .option('-s --slug <slug>', 'TODO: install module under slug', 'mp-admin-index')
   .action((moduleName, params) => {
-    const path = 'marketplace_builder/pages/' + moduleName;
-    shell.mkdir('-p', path);
-    shell.cp(__dirname + '/gui/src/mp-admin.liquid', path + '/index.liquid');
+    const targetPath = `marketplace_builder/pages/${moduleName}`;
+    shell.mkdir('-p', targetPath);
+    shell.cp(__dirname + '/gui/src/mp-admin.liquid', `${targetPath}/index.liquid`);
     shell.cp(__dirname + '/gui/public/app.js', 'marketplace_builder/assets/mp-admin-app.js');
 
     logger.Success('GUI installed under pages/' + moduleName);
