31ed1984f11eeff9bb67fdb3e827fcad1cf8588b
Ignacy Moryc <imoryc@protonmail.com>
2018-10-29 15:42:10 +0100

    Prettier-eslint

--- marketplace-kit-archive.js
+++ marketplace-kit-archive.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const program = require("commander"),
-  fs = require("fs"),
-  shell = require("shelljs"),
-  archiver = require("archiver"),
-  logger = require("./lib/kit").logger,
-  validate = require("./lib/validators"),
-  version = require("./package.json").version;
+const program = require('commander'),
+  fs = require('fs'),
+  shell = require('shelljs'),
+  archiver = require('archiver'),
+  logger = require('./lib/kit').logger,
+  validate = require('./lib/validators'),
+  version = require('./package.json').version;
 
 const checkDirectory = directoryPath => {
   validate.directoryExists({
@@ -15,34 +15,33 @@ const checkDirectory = directoryPath => {
   });
   validate.directoryEmpty({
     path: directoryPath,
-    message:
-      "marketplace_builder is empty. Proceeding would remove everything from your marketplace."
+    message: 'marketplace_builder is empty. Proceeding would remove everything from your marketplace.'
   });
 };
 
 const makeArchive = (path, directory, withoutAssets) => {
   checkDirectory(directory);
 
-  shell.mkdir("-p", "tmp");
-  shell.rm("-rf", path);
+  shell.mkdir('-p', 'tmp');
+  shell.rm('-rf', path);
 
   const output = fs.createWriteStream(path);
-  const archive = archiver("zip", { zlib: { level: 6 } });
+  const archive = archiver('zip', { zlib: { level: 6 } });
 
   // listen for all archive data to be written
   // 'close' event is fired only when a file descriptor is involved
-  output.on("close", () => {
+  output.on('close', () => {
     const sizeInMB = archive.pointer() / 1024 / 1024;
     logger.Info(`Archive size: ${sizeInMB.toFixed(2)} MB`);
   });
 
-  archive.on("warning", err => {
-    if (err.code === "ENOENT") {
+  archive.on('warning', err => {
+    if (err.code === 'ENOENT') {
       logger.Error(err);
     } else throw err;
   });
 
-  archive.on("error", err => {
+  archive.on('error', err => {
     throw err;
   });
 
@@ -53,27 +52,15 @@ const makeArchive = (path, directory, withoutAssets) => {
     // Add all files to archive, exclude assets which are deployed straight to S3
     // For modules for now we go with the old aproach (not through S3) to avoid problems
     // with deep nesting
-    archive.glob(
-      "**/*",
-      { cwd: directory, ignore: ["assets/**"] },
-      { prefix: directory }
-    );
-    archive.glob(
-      "**/*",
-      { cwd: "public", ignore: ["assets/**"] },
-      { prefix: "public" }
-    );
-    archive.glob(
-      "**/*",
-      { cwd: "private", ignore: ["assets/**"] },
-      { prefix: "private" }
-    );
-    archive.glob("**/*", { cwd: "modules" }, { prefix: "modules" });
+    archive.glob('**/*', { cwd: directory, ignore: ['assets/**'] }, { prefix: directory });
+    archive.glob('**/*', { cwd: 'public', ignore: ['assets/**'] }, { prefix: 'public' });
+    archive.glob('**/*', { cwd: 'private', ignore: ['assets/**'] }, { prefix: 'private' });
+    archive.glob('**/*', { cwd: 'modules' }, { prefix: 'modules' });
   } else {
     archive.directory(directory, true);
-    archive.directory("public", true);
-    archive.directory("private", true);
-    archive.directory("modules", true);
+    archive.directory('public', true);
+    archive.directory('private', true);
+    archive.directory('modules', true);
   }
 
   // finalize the archive (ie we are done appending files but streams have to finish yet)
@@ -83,13 +70,9 @@ const makeArchive = (path, directory, withoutAssets) => {
 
 program
   .version(version)
-  .option("--dir <dir>", "files to be added to build", "marketplace_builder")
-  .option("--without-assets", "if present assets directory will be excluded")
-  .option(
-    "--target <target>",
-    "path to archive",
-    process.env.TARGET || "./tmp/marketplace-release.zip"
-  )
+  .option('--dir <dir>', 'files to be added to build', 'marketplace_builder')
+  .option('--without-assets', 'if present assets directory will be excluded')
+  .option('--target <target>', 'path to archive', process.env.TARGET || './tmp/marketplace-release.zip')
   .parse(process.argv);
 
 makeArchive(program.target, program.dir, program.withoutAssets);
