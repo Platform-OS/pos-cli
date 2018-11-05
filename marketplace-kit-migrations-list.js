266b577ea170449005afce1704a1186efaeb5ab2
Paweł Kowalski <pavelloz@gmail.com>
2018-11-05 19:55:39 +0100

    Refactor migrations list

--- marketplace-kit-migrations-list.js
+++ marketplace-kit-migrations-list.js
@@ -6,32 +6,20 @@ const program = require('commander'),
   fetchAuthData = require('./lib/settings').fetchSettings,
   version = require('./package.json').version;
 
+const logMigration = migration => {
+  logger.Info(`[${migration.id}] Name: ${migration.name} - Status: ${migration.state} - Errors: (${migration.error_messages})`);
+};
+
 program
   .version(version)
   .arguments('<environment>', 'name of the environment. Example: staging')
-  .option(
-    '-c --config-file <config-file>',
-    'config file path',
-    '.marketplace-kit'
-  )
+  .option('-c --config-file <config-file>', 'config file path', '.marketplace-kit')
   .action((environment, params) => {
     process.env.CONFIG_FILE_PATH = params.configFile;
     const authData = fetchAuthData(environment);
     const gateway = new Gateway(authData);
 
-    gateway.listMigrations().then(
-      body => {
-        logger.Success(`[Migration - List]`);
-        body['migrations'].map(migration => {
-          logger.Info(
-            `${migration['name']} - ${migration['state']} Errors: (${migration['error_messages']})`
-          );
-        });
-      },
-      error => {
-        logger.Error(`[Migration - List] Error ${error.message}`);
-      }
-    );
+    gateway.listMigrations().then(response => response.migrations.reverse().map(logMigration));
   });
 
 program.parse(process.argv);
