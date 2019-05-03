8a00dd0240fe579e0d807cf6ccd7a8d71271e133
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2019-05-03 11:44:47 +0200

    Data clean: add inline confirmation option

--- marketplace-kit-data-clean.js
+++ marketplace-kit-data-clean.js
@@ -6,38 +6,47 @@ const program = require('commander'),
   logger = require('./lib/logger'),
   fetchAuthData = require('./lib/settings').fetchSettings,
   version = require('./package.json').version;
+const confirmationText = 'CLEAN DATA';
 
 const clean = (gateway, confirmation) => {
   logger.Info('Going to clean data');
   gateway
-    .dataClean(confirmation)
+    .dataClean(confirmationText)
     .then(() => logger.Success('Instance data cleaned.'))
     .catch({ statusCode: 404 }, () => logger.Error('[404] Data clean is not supported by the server'));
 };
 
-async function confirmCleanup(gateway) {
-  const confirmationText = 'CLEAN DATA';
-  const message = `WARNING!!! You are going to REMOVE your data.
-There is no comming back.
-If you still want to continue please type: '${confirmationText}' `;
+const promptConfirmation = async(confirmationText) => {
+  const message = `If you still want to continue please type: '${confirmationText}' `;
 
   const response = await prompts({ type: 'text', name: 'confirmation', message: message });
-  if (response.confirmation == confirmationText) {
-    clean(gateway, response.confirmation);
+  return response.confirmation;
+};
+
+const confirmCleanup = async(gateway, inlineConfirmation) => {
+  logger.Warn('');
+  logger.Warn(`WARNING!!! You are going to REMOVE your data from instance: ${gateway.url}`);
+  logger.Warn('There is no comming back.');
+  logger.Warn('');
+  const confirmed = inlineConfirmation || (await promptConfirmation(confirmationText)) == confirmationText;
+  if (confirmed) {
+    clean(gateway);
   } else {
-    logger.Info('Closed without cleaning instance data.');
+    logger.Error('Wrong confirmation. Closed without cleaning instance data.');
+    process.exit(1);
   }
-}
+};
 
 program
   .version(version)
   .arguments('[environment]', 'name of the environment. Example: staging')
+  .option('--auto-confirm', 'auto confirm instance clean without prompt')
   .option('-c --config-file <config-file>', 'config file path', '.marketplace-kit')
   .action((environment, params) => {
     process.env.CONFIG_FILE_PATH = params.configFile;
     const gateway = new Gateway(fetchAuthData(environment, program));
 
-    confirmCleanup(gateway);
+    confirmCleanup(gateway, params.autoConfirm);
   });
 
 program.parse(process.argv);
---
+++ test/lib/data-clean.test.js
@@ -0,0 +1,10 @@
+var sh = require('shelljs');
+var authEnvs = 'MPKIT_URL=http://example.com MPKIT_TOKEN=1234 MPKIT_EMAIL=foo@example.com';
+
+test('should show message when wrong confirmation passed inline', () => {
+  sh.config.silent = true;
+  let command = sh.exec(`echo "wrong confirm" | ${authEnvs} ./marketplace-kit.js data clean`);
+
+  expect(command.code).toEqual(1);
+  expect(command.stderr).toEqual(expect.stringContaining('Wrong confirmation. Closed without cleaning instance data.'));
+});
