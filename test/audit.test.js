819972098352a5edd26646712a161acdb045a876
Paweł Kowalski <pavelloz@gmail.com>
2019-08-02 12:42:38 +0200

    Use js file directly, not via system command

--- test/audit.test.js
+++ test/audit.test.js
@@ -5,14 +5,14 @@ const fixtures = `${c}/test/fixtures/audit`;
 const cwd = name => `${fixtures}/${name}`;
 
 test('Reports no errors with empty directory', async () => {
-  const { stdout, exitCode } = await execa.command('pos-cli audit', { cwd: cwd('empty') });
+  const { stdout, exitCode } = await execa.command('./bin/pos-cli-audit.js', { cwd: cwd('empty') });
   expect(exitCode).toBe(0);
   expect(stdout).toMatch('[Audit] 0 rules detected issues.');
 });
 
 describe('Audit - app directory', () => {
   test('Reports 1 error in app', async () => {
-    const { stdout, exitCode } = await execa.command('pos-cli audit', { cwd: cwd('oneError') });
+    const { stdout, exitCode } = await execa.command('./bin/pos-cli-audit.js', { cwd: cwd('oneError') });
     expect(exitCode).toBe(0);
     expect(stdout).toMatch('[Audit] 1 rule detected issues.');
     expect(stdout).toMatch('enable_profiler: true');
@@ -20,7 +20,7 @@ describe('Audit - app directory', () => {
   });
 
   test('Reports 2 different errors in one file', async () => {
-    const { stdout, exitCode } = await execa.command('pos-cli audit', { cwd: cwd('twoErrors') });
+    const { stdout, exitCode } = await execa.command('./bin/pos-cli-audit.js', { cwd: cwd('twoErrors') });
     expect(exitCode).toBe(0);
     expect(stdout).toMatch('[Audit] 2 rules detected issues.');
     expect(stdout).toMatch('enable_profiler: true');
@@ -29,7 +29,7 @@ describe('Audit - app directory', () => {
   });
 
   test('Reports 3 different errors in two files', async () => {
-    const { stdout, exitCode } = await execa.command('pos-cli audit', { cwd: cwd('threeErrors') });
+    const { stdout, exitCode } = await execa.command('./bin/pos-cli-audit.js', { cwd: cwd('threeErrors') });
     expect(exitCode).toBe(0);
     expect(stdout).toMatch('[Audit] 3 rules detected issues.');
     expect(stdout).toMatch('enable_profiler: true');
@@ -41,7 +41,7 @@ describe('Audit - app directory', () => {
 
 describe('Audit - marketplace_builder directory', () => {
   test('Reports 3 different errors in two files', async () => {
-    const { stdout, exitCode } = await execa.command('pos-cli audit', { cwd: cwd('threeErrors_mpb') });
+    const { stdout, exitCode } = await execa.command('./bin/pos-cli-audit.js', { cwd: cwd('threeErrors_mpb') });
     expect(exitCode).toBe(0);
     expect(stdout).toMatch('[Audit] 3 rules detected issues.');
     expect(stdout).toMatch('enable_profiler: true');
@@ -54,7 +54,7 @@ describe('Audit - marketplace_builder directory', () => {
 describe('Audit - modules directory', () => {
 
   test('Reports errors for modules files', async () => {
-    const { stdout, exitCode } = await execa.command('pos-cli audit', { cwd: cwd('modules') });
+    const { stdout, exitCode } = await execa.command('./bin/pos-cli-audit.js', { cwd: cwd('modules') });
     expect(exitCode).toBe(0);
     expect(stdout).toMatch('[Audit] 3 rules detected issues.');
     expect(stdout).toMatch('enable_profiler: true');
