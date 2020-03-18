25b0b63b4727b5f337724e9c17c4034f6f9e4b57
Kornel Lugosi <coornail@gmail.com>
2020-03-18 13:43:11 +0100

    Add command to pull a module

--- bin/pos-cli-modules.js
+++ bin/pos-cli-modules.js
@@ -6,4 +6,5 @@ program
   .name('pos-cli modules')
   .command('remove [environment] <name>', 'remove module from instance (removes configuration and data)')
   .command('list [environment]', 'list installed modules')
+  .command('pull [module] [environment]', 'pull a module')
   .parse(process.argv);
