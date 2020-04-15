7a27dfd2f294366ff9cad94a55fbc2128d7a0c90
Kornel Lugosi <coornail@gmail.com>
2020-04-15 10:21:10 +0200

    Add ' and & as valid filename characters

--- test/valid-file-path.test.js
+++ test/valid-file-path.test.js
@@ -5,7 +5,8 @@ describe('Linux - Correct', () => {
     'a/my_images/IMG--__23~123.min.jpeg',
     'modules/admin/foo_bar/test--__23~123.html.liquid',
     'modules/admin cms/foo bar/test (Screenshot)%20@.liquid',
-    'a/foo/test+99.zip'
+    'a/foo/test+99.zip',
+    "modules/homepage/public/assets/images/blog/second-half-2016/Beginner's-Running-Shoes.jpg"
   ];
 
   correct.forEach(p => {
@@ -38,7 +39,9 @@ describe('Windows - Correct', () => {
     'a\\my_images\\IMG--__23~123.min.jpeg',
     'modules\\admin\\foo_bar\\test--__23~123.html.liquid',
     'modules\\admin cms\\foo bar\\test (Screenshot)%20@.liquid',
-    'a\\foo\\test+99.zip'
+    'a\\foo\\test+99.zip',
+    "modules\\homepage\\public\\assets\\images\\blog\\second-half-2016\\Beginner's-Running-Shoes.jpg",
+    'modules\\homepage\\public\\assets\\images\\blog\\2017\\Training for Long Distance Swimming & Triathlons.jpg'
   ];
   correct.forEach(p => {
     test(p, () => {
