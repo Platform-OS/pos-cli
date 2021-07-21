08e18f7530ecc58089b5d93c6e570a66f3e145a9
Pawel Kowalski <pavelloz@gmail.com>
2021-07-21 15:37:52 +0200

    Remove comments

--- gui/admin/src/pages/Logs/store.js
+++ gui/admin/src/pages/Logs/store.js
@@ -1,34 +1,5 @@
 import { writable } from 'svelte/store';
 
-// const isHighlighted = (item) => {
-//   item.isHighlighted = !!item.error_type.match(/error/i);
-// };
-
-// function createStore() {
-//   const { subscribe, get, set, update } = writable({
-//     cachedLastId: null,
-//     lastId: null,
-//     logs: []
-//   });
-
-//   return {
-//     subscribe,
-//     set: obj => set(() => {
-//       return {
-//         cachedLastId: obj.cachedLastId,
-//         lastId: obj.lastId,
-//         logs: obj.logs
-//       }
-//     }),
-//     add: obj => update(store => {
-//       return {
-//         ...obj,
-//         logs: store.logs.concat(obj.logs)
-//       }
-//     })
-//   };
-// }
-
 export const logs = writable([]);
 export const cachedLastId = writable(null);
 export const lastId = writable(null);
