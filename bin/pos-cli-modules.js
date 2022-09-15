004124ba8e986f927291015e5e779e2d13a203c9
Kalman Hosszu <hosszu.kalman@gmail.com>
2022-09-15 14:07:30 +0200

    Define a new command to install a module from github

--- bin/pos-cli-modules.js
+++ bin/pos-cli-modules.js
@@ -8,4 +8,5 @@ program
   .command('list [environment]', 'list installed modules')
   .command('pull [environment] <name>', 'pull a module')
   .command('init <name>', 'initialize a module with the starter structure')
+  .command('install <name>', 'download a platformOS module from github. Do not use "pos-module-" prefix')
   .parse(process.argv);
