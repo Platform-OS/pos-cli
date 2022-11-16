f966b3d213a1704903966cdf73657909a2b7a909
piotrze <piotr.bliszczyk@gmail.com>
2022-11-16 15:01:23 +0100

    wip

--- bin/pos-cli-modules.js
+++ bin/pos-cli-modules.js
@@ -6,7 +6,8 @@ program
   .name('pos-cli modules')
   .command('remove [environment] <name>', 'remove module from instance (removes configuration and data)')
   .command('list [environment]', 'list installed modules')
-  .command('pull [environment] <name>', 'pull a module')
+  .command('pull [environment] <name>', 'pull a module for instance')
   .command('init <name>', 'initialize a module with the starter structure')
   .command('version <name> [version] --package', 'create a new version of the module')
+  .command('upload <name>', 'upload module version')
   .parse(process.argv);
