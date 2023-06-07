a4b3d423cbf69e038863498b2f2eb8227fffdfec
piotrze <piotr.bliszczyk@gmail.com>
2023-06-07 14:21:05 +0200

    Use generator from external source

--- bin/pos-cli-generate.js
+++ bin/pos-cli-generate.js
@@ -1,18 +1,68 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const program = require("commander");
+const yeoman = require("yeoman-environment");
+const yeomanEnv = yeoman.createEnv();
+const path = require("path");
+const chalk = require("chalk");
+const dir = require('../lib/directories');
+const compact = require('lodash.compact');
 
+const runYeoman = (generatorPath, attributes) => {
+  try {
+    const generatorName = path.basename(generatorPath);
+    const generatorPathFull = `./${generatorPath}/index.js`;
+    yeomanEnv.register(generatorPathFull, generatorName);
+
+    const generatorArgs = compact([generatorName, attributes]);
+    yeomanEnv.run(generatorArgs, {});
+  } catch (e) {
+    // TODO: catch the error from yeoman?
+    console.log('foo');
+    console.error(chalk.red("Error: "));
+    console.error(e);
+  }
+}
+
+const showHelpForGenerator = (generatorPath) => {
+  const generatorName = path.basename(generatorPath);
+  const generatorPathFull = `./${generatorPath}/index.js`;
+  yeomanEnv.register(generatorPathFull, generatorName);
+
+  const generator = yeomanEnv.get(generatorName);
+  const generatorInstance = yeomanEnv.instantiate(generator, ['']);
+  console.log(`Generator: ${generatorName}`);
+  console.log(`  ${generatorInstance.description}`);
+  console.log(`\nUsage: `);
+  const usage = generatorInstance._arguments.map(arg => `<${arg.name}> `)
+  console.log(
+    `  pos-cli generate ${generatorPath} ${usage}`
+  );
+  console.log('\nArguments:');
+  console.log(generatorInstance.argumentsHelp());
+  console.log('');
+}
+
+const description = `Run generator
+  Example:
+
+    pos-cli generate modules/core/generators/<generator_name> <generator_attribues>`;
 program
   .name('pos-cli generate')
-  .command('command', 'Generate command with build and check files')
-  .command('resource', 'Generate model and endpoints files for create, read, update and delete')
-  .command('rest-api', 'Generate rest api endpoints files')
-  .parse(process.argv);
+  .description(description)
+  .arguments('<generatorPath> [generatorAttributes]')
+  // .argument('<generatorPath>', 'path to the generator directory')
+  // .argument('[generatorAttributes]', 'arguments that will be passed to the generator')
+  .option('--generator-help', 'show help for given generator')
+  .usage("<generatorPath> [generatorAttributes]")
+  .action(function (generatorPath, generatorAttributes, options) {
+    if (options.generatorHelp){
+      showHelpForGenerator(generatorPath);
+    } else {
+      runYeoman(generatorPath, generatorAttributes);
+    }
+  });
 
-const commandList = Object.keys(program._execs);
-if (!commandList.includes(program.args[0])) {
-  program.outputHelp();
-  console.log(`unknown command: ${program.args[0]}`);
-}
+program.parse(process.argv);
 
 if (!program.args.length) program.help();
