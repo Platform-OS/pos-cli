3b6ee50121a98e883383248f491a531358a9779f
Łukasz Krysiewicz <zx@secondgate.pl>
2023-07-06 10:32:10 +0200

    Users tool now uses new aside component

--- gui/next/src/routes/users/[id]/+page.svelte
+++ gui/next/src/routes/users/[id]/+page.svelte
@@ -9,6 +9,7 @@ import { page } from '$app/stores';
 import { user } from '$lib/api/user.js';
 
 import Icon from '$lib/ui/Icon.svelte';
+import Aside from '$lib/ui/Aside.svelte';
 
 
 // properties
@@ -154,38 +155,30 @@ time {
   <title>{item?.email ?? 'Users'} | platformOS</title>
 </svelte:head>
 
+<Aside title={item?.email ?? item?.id ?? 'Loading…'} closeUrl="/users?{$page.url.searchParams.toString()}">
 
-<aside transition:appear>
-  <div class="container">
-    <header>
-      <div>
-        <h2>{item?.email ?? item?.id ?? 'Loading…'}</h2>
-        <div class="info">
-          {#if item?.id}ID: {item?.id}{/if}
-          <time datetime={item?.created_at}>
-            {(new Date(item?.created_at)).toLocaleDateString(undefined, {})}
-            {(new Date(item?.created_at)).toLocaleTimeString(undefined, {})}
-          </time>
-        </div>
-      </div>
-      <a href="/users">
-        <span class="label">Close user info</span>
-        <Icon icon="x" />
-      </a>
-    </header>
-
-    <dl class="tech">
-      {#if item?.external_id}<dt>External ID:</dt> <dd>{item?.external_id}</dd>{/if}
-      {#if item?.jwt_token}<dt>JWT:</dt> <dd>{item?.jwt_token}</dd>{/if}
-    </dl>
-
-    <dl class="personal">
-      {#if item?.name}<div><dt>First name:</dt> <dd>{item?.name}</dd></div>{/if}
-      {#if item?.first_name}<div><dt>First name:</dt> <dd>{item?.first_name}</dd></div>{/if}
-      {#if item?.middle_name}<div><dt>Middle name:</dt> <dd>{item?.middle_name}</dd></div>{/if}
-      {#if item?.last_name}<div><dt>Last name:</dt> <dd>{item?.last_name}</dd></div>{/if}
-      {#if item?.slug}<div><dt>Slug:</dt> <dd>{item?.slug}</dd></div>{/if}
-      {#if item?.language}<div><dt>Language:</dt> <dd>{item?.language}</dd></div>{/if}
-    </dl>
+  <div>
+    <div class="info">
+      {#if item?.id}ID: {item?.id}{/if}
+      <time datetime={item?.created_at}>
+        {(new Date(item?.created_at)).toLocaleDateString(undefined, {})}
+        {(new Date(item?.created_at)).toLocaleTimeString(undefined, {})}
+      </time>
+    </div>
   </div>
-</aside>
+
+  <dl class="tech">
+    {#if item?.external_id}<dt>External ID:</dt> <dd>{item?.external_id}</dd>{/if}
+    {#if item?.jwt_token}<dt>JWT:</dt> <dd>{item?.jwt_token}</dd>{/if}
+  </dl>
+
+  <dl class="personal">
+    {#if item?.name}<div><dt>First name:</dt> <dd>{item?.name}</dd></div>{/if}
+    {#if item?.first_name}<div><dt>First name:</dt> <dd>{item?.first_name}</dd></div>{/if}
+    {#if item?.middle_name}<div><dt>Middle name:</dt> <dd>{item?.middle_name}</dd></div>{/if}
+    {#if item?.last_name}<div><dt>Last name:</dt> <dd>{item?.last_name}</dd></div>{/if}
+    {#if item?.slug}<div><dt>Slug:</dt> <dd>{item?.slug}</dd></div>{/if}
+    {#if item?.language}<div><dt>Language:</dt> <dd>{item?.language}</dd></div>{/if}
+  </dl>
+
+</Aside>
