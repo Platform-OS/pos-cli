c09c03665205774544e9a3d43521a814a95e17c2
dariusz gorzeba <dgorzeba@gmail.com>
2023-09-20 21:56:14 +0200

    better slack alert

--- bin/pos-cli-logsv2-searchAround.js
+++ bin/pos-cli-logsv2-searchAround.js
@@ -7,23 +7,21 @@ const program = require('commander'),
 program
   .name('pos-cli logsv2 search')
   .arguments('[environment]', 'name of environment. Example: staging')
-  .option('--sql <sql>', 'SQL query to fetch logs')
+  .option('--stream_name <stream_name>', 'stream name to search around in', 'logs')
+  .option('--key <key>', 'key')
   .option('--size <size>', 'rows size', 10)
-  .option('--from <from>', 'start from', 0)
-  .option('--start_time <st>', 'starttime')
-  .option('--end_time <et>', 'endtime')
   .option('--json', 'output as json')
   .action(async (environment) => {
     try {
       const client = await swagger.SwaggerProxy.client(environment);
-      const response = await client.searchSQL(program)
+      const response = await client.searchAround(program)
 
       if (!program.json)
         swagger.search.printLogs(response)
       else
         console.log(response.body)
 
-    } catch(e) { console.error(e) }
+    } catch(e) { console.log(e) }
   });
 
 program.parse(process.argv);
--- bin/pos-cli-logsv2.js
+++ bin/pos-cli-logsv2.js
@@ -5,5 +5,6 @@ const program = require('commander');
 program
   .name('pos-cli logsv2')
   .command('search', 'search logs')
+  .command('searchAround', 'search stream for records around timestamp')
   .command('alerts', 'manage alerts')
   .parse(process.argv);
