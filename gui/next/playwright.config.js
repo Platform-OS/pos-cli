230cf00832af2b68228969f8006222229476af1d
Łukasz Krysiewicz <zx@secondgate.pl>
2023-10-09 19:29:17 +0200

    Better Playwirght config

--- gui/next/playwright.config.js
+++ gui/next/playwright.config.js
@@ -16,19 +16,16 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? undefined : undefined,
+  retries: process.env.CI ? 1 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:4173/',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
+    /* save screenshots when failed, only on CI */
     screenshot: process.env.CI ? 'only-on-failure' : 'off'
   },
 
