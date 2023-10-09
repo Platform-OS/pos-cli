134f36255b84a43505eddecdc995194fbe575b10
Łukasz Krysiewicz <zx@secondgate.pl>
2023-10-09 16:53:58 +0200

    Experimenting with wait

--- gui/next/playwright/backgroundJobs.spec.js
+++ gui/next/playwright/backgroundJobs.spec.js
@@ -17,6 +17,7 @@ test('see home screen', async ({ page }) => {
 test('viewing scheduled background jobs', async ({ page }) => {
   await page.goto(triggerLogUrl + 'background_job');
   await expect(page.getByText('background job scheduled')).toBeVisible();
+  await page.waitForTimeout(4000);
 
   await page.goto(url);
 
@@ -29,6 +30,7 @@ test('viewing scheduled background jobs', async ({ page }) => {
 test('viewing background job details', async ({ page }) => {
   await page.goto(triggerLogUrl + 'background_job');
   await expect(page.getByText('background job scheduled')).toBeVisible();
+  await page.waitForTimeout(4000);
 
   await page.goto(url);
 
@@ -45,6 +47,7 @@ test('viewing background job details', async ({ page }) => {
 test('deleting scheduled background job', async ({ page }) => {
   await page.goto(triggerLogUrl + 'background_job_to_delete');
   await expect(page.getByText('background job scheduled')).toBeVisible();
+  await page.waitForTimeout(4000);
 
   page.on('dialog', async dialog => {
     expect(dialog.message()).toEqual('Are you sure you want to delete this background job?');
