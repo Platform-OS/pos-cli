8ee1765adfed758b34a20e274d352f243d0c7205
Łukasz Krysiewicz <zx@secondgate.pl>
2023-10-09 19:29:44 +0200

    More workers

--- gui/next/playwright.config.js
+++ gui/next/playwright.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
+  workers: 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
