6fd1c80ac847ccecaa5774c491f50afcd3707961
piotrze <piotr.bliszczyk@gmail.com>
2023-10-19 16:49:03 +0200

    Update install help message

--- bin/pos-cli-modules.js
+++ bin/pos-cli-modules.js
@@ -8,7 +8,7 @@ program
   .command('list [environment]', 'list installed modules on the instance')
   .command('pull [environment] <name>', 'pull a module for the instance')
   .command('remove [environment] <name>', 'remove module from the instance (removes configuration and data)')
-  .command('install', 'adds module to pos-modules file and resolve dependencies')
+  .command('install', 'resolve dependencies from pos-modules.json')
   .command('init <name>', 'initialize a module with the starter structure')
   .command('version [version] --package', 'create a new version of the module')
   .command('push', 'publish module version')
