d1e58f88f235dc06199f68b8cd631d7b6bb6b5e1
piotrze <piotr.bliszczyk@gmail.com>
2023-12-13 12:00:28 +0100

    Remove log; update help

--- bin/pos-cli-modules.js
+++ bin/pos-cli-modules.js
@@ -7,7 +7,7 @@ program.showHelpAfterError();
 program
   .name('pos-cli modules')
   .command('list [environment]', 'list installed modules on the instance')
-  .command('pull [environment] <name>', 'pull a module for the instance')
+  .command('pull <name>', 'fetch module code')
   .command('remove [environment] <name>', 'remove module from the instance (removes configuration and data)')
   .command('install [module-with-version]', 'Add new modules and resolve dependencies from pos-modules.json')
   .command('init <name>', 'initialize a module with the starter structure')
