ba1469e342e3685ea53fdab804b71cc2e0fd6b53
piotrze <piotr.bliszczyk@gmail.com>
2023-12-13 11:54:15 +0100

    Modules pull public package without env

--- bin/pos-cli-modules-pull.js
+++ bin/pos-cli-modules-pull.js
@@ -1,41 +1,46 @@
 #!/usr/bin/env node
 
-const program = require('commander'),
-  ora = require('ora'),
-  Gateway = require('../lib/proxy'),
-  logger = require('../lib/logger'),
-  fetchAuthData = require('../lib/settings').fetchSettings,
-  downloadFile = require('../lib/downloadFile'),
-  waitForStatus = require('../lib/data/waitForStatus');
+const shell = require('shelljs');
+const program = require('commander');
+const ora = require('ora');
+const Gateway = require('../lib/proxy');
+const logger = require('../lib/logger');
+const fetchAuthData = require('../lib/settings').fetchSettings;
+const downloadFile = require('../lib/downloadFile');
+const waitForStatus = require('../lib/data/waitForStatus');
 const spinner = ora({ text: 'Exporting', stream: process.stdout, spinner: 'bouncingBar' });
 const { unzip } = require('../lib/unzip');
-const shell = require('shelljs');
+const Portal = require('../lib/portal');
 
 program
   .name('pos-cli modules pull')
-  .arguments('[environment]', 'name of the environment. Example: staging')
-  .arguments('[module]', 'module name to pull')
-  .action((environment, module, params) => {
+  .arguments('[module]', 'module name to pull, ex. core, core@1.0.0')
+  .option('--env <env>', 'name of the environment. Example: staging')
+  .action((module, params) => {
     const filename = 'modules.zip';
-    const authData = fetchAuthData(environment, program);
-    let gateway = new Gateway(authData);
     spinner.start();
-    gateway
-      .appExportStart({ module_name: module })
-      .then(exportTask => {
-        waitForStatus(() => gateway.appExportStatus(exportTask.id), 'ready_for_export', 'success')
-          .then(exportTask => downloadFile(exportTask.zip_file.url, filename))
-          .then(() => unzip(filename, process.cwd()))
-          .then(() => shell.rm(filename))
-          .then(() => spinner.succeed('Downloading files'))
-          .catch(error => {
-            logger.Debug(error);
-            spinner.fail('Export failed');
-          });
-      })
+    let flow;
+    const environment = params.env;
+    if (environment){
+      const authData = fetchAuthData(environment, program);
+      const gateway = new Gateway(authData);
+      flow = gateway
+        .appExportStart({ module_name: module })
+        .then(exportTask => waitForStatus(() => gateway.appExportStatus(exportTask.id), 'ready_for_export', 'success'))
+        .then(exportTask => downloadFile(exportTask.zip_file.url, filename))
+        .then(() => unzip(filename, process.cwd()))
+    } else {
+      flow = Portal.moduleVersionsSearch(module)
+        .then(moduleVersion => downloadFile(moduleVersion['public_archive'], filename))
+        .then(() => unzip(filename, `${process.cwd()}/modules`))
+    }
+
+    flow
+      .then(() => shell.rm(filename))
+      .then(() => spinner.succeed('Downloading files'))
       .catch({ statusCode: 404 }, () => {
         spinner.fail('Export failed');
-        logger.Error('[404] Data export is not supported by the server');
+        logger.Error('[404] Module not found');
       })
       .catch(e => {
         spinner.fail('Export failed');
