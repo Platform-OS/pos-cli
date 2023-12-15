b8f31a4566f1b5f196178d61c07e3168dbd2837c
piotrze <piotr.bliszczyk@gmail.com>
2023-12-15 10:34:29 +0100

    Update bin/pos-cli-modules.js
    Co-authored-by: Maciej Krajowski-Kukiel <krajek6@gmail.com>

--- bin/pos-cli-modules.js
+++ bin/pos-cli-modules.js
@@ -7,7 +7,7 @@ program.showHelpAfterError();
 program
   .name('pos-cli modules')
   .command('list [environment]', 'list installed modules on the instance')
-  .command('pull <name>', 'fetch module code')
+  .command('pull <name>', 'pull module code to your disk')
   .command('remove [environment] <name>', 'remove module from the instance (removes configuration and data)')
   .command('install [module-with-version]', 'Add new modules and resolve dependencies from pos-modules.json')
   .command('init <name>', 'initialize a module with the starter structure')
