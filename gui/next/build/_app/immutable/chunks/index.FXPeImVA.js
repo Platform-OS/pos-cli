fb7937856f5da6fcc40df5db13fceef66a4ea418
Łukasz Krysiewicz <zx@secondgate.pl>
2023-12-19 11:26:51 +0100

    Updated to SvelteKit2!

---
+++ gui/next/build/_app/immutable/chunks/index.FXPeImVA.js
@@ -0,0 +1,1 @@
+import{Y as n}from"./scheduler.C43H4T0F.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
--- gui/next/build/_app/immutable/nodes/9.c260b96d.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.771e17c8.js";import{S as t,i as e}from"../chunks/index.25a56fcb.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
