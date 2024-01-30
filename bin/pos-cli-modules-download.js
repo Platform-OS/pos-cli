61036463fc6f457120558a61e476653b6e2e568f
piotrze <piotr.bliszczyk@gmail.com>
2024-01-30 11:51:31 +0100

    Add command modules download (#583)
    Bring back old functionality to module pull command

--- bin/pos-cli-modules-download.js
+++ bin/pos-cli-modules-download.js
@@ -5,25 +5,22 @@ const program = require('commander');
 const ora = require('ora');
 const Gateway = require('../lib/proxy');
 const logger = require('../lib/logger');
-const fetchAuthData = require('../lib/settings').fetchSettings;
 const downloadFile = require('../lib/downloadFile');
-const waitForStatus = require('../lib/data/waitForStatus');
 const spinner = ora({ text: 'Exporting', stream: process.stdout, spinner: 'bouncingBar' });
 const { unzip } = require('../lib/unzip');
+const Portal = require('../lib/portal');
 
 program
-  .name('pos-cli modules pull')
-  .arguments('[environment]', 'name of the environment. Example: staging')
-  .arguments('[module]', 'module name to pull')
-  .action((environment, module, params) => {
+  .name('pos-cli modules download')
+  .arguments('<module>', 'module name, ex. core, core@1.0.0')
+  .action((module, params) => {
     const filename = 'modules.zip';
     spinner.start();
-    const authData = fetchAuthData(environment, program);
-    const gateway = new Gateway(authData);
-    gateway
-      .appExportStart({ module_name: module })
-      .then(exportTask => waitForStatus(() => gateway.appExportStatus(exportTask.id), 'ready_for_export', 'success'))
-      .then(exportTask => downloadFile(exportTask.zip_file.url, filename))
+    const environment = params.env;
+    Portal.moduleVersionsSearch(module)
+      .then(moduleVersion => downloadFile(moduleVersion['public_archive'], filename))
+      .then(() => unzip(filename, `${process.cwd()}/modules`))
+      .then(() => shell.rm(filename))
       .then(() => spinner.succeed('Downloading files'))
       .catch({ statusCode: 404 }, () => {
         spinner.fail('Export failed');
