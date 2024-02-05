30249b672913c2e06cb1eb8189ba4d3ef14ec6d1
piotrze <piotr.bliszczyk@gmail.com>
2024-02-05 18:33:37 +0100

    modules install: do not update version to the newest available if version is defined

--- bin/pos-cli-modules-install.js
+++ bin/pos-cli-modules-install.js
@@ -3,59 +3,40 @@
 const program = require('commander');
 const ora = require('ora');
 const semver = require('semver');
-const fs = require('fs');
-const path = require('path');
 const logger = require('../lib/logger');
 const fetchAuthData = require('../lib/settings').fetchSettings;
 const spinner = ora({ text: 'Setup', stream: process.stdout, spinner: 'bouncingBar' });
-const files = require('../lib/files');
+const configFiles = require('../lib/modules/configFiles');
 const { findModuleVersion, resolveDependencies } = require('../lib/modules/dependencies')
 const Portal = require('../lib/portal');
 
-const posModulesFilePath = 'app/pos-modules.json';
-const posModulesLockFilePath = 'app/pos-modules.lock.json';
-
-const readLocalModules = () => {
-  const config = files.readJSON(posModulesFilePath, { throwDoesNotExistError: true });
-  return config['modules'] || {};
-};
-
 const addNewModule = async (moduleName, moduleVersion, localModules, getVersions) => {
   const newModule = await findModuleVersion(moduleName, moduleVersion, getVersions);
+  let modules;
   if(newModule){
-    const modules = {...localModules, ...newModule};
+    if (moduleVersion || !localModules[moduleName]) {
+      modules = {...localModules, ...newModule};
+    } else {
+      modules = {...localModules };
+    }
     return modules;
   } else {
     throw new Error(`Can't find module ${moduleName} with version ${moduleVersion}`);
   }
 };
 
-const writePosModules = (modules) => {
-  fs.writeFileSync(
-    path.join(process.cwd(), posModulesFilePath),
-    JSON.stringify({ modules: modules }, null, 2)
-  );
-}
-
-const writePosModulesLock = (modules) => {
-  fs.writeFileSync(
-    path.join(process.cwd(), posModulesLockFilePath),
-    JSON.stringify({ modules: modules }, null, 2)
-  );
-}
-
 program
-  .name('pos-cli modules setup')
+  .name('pos-cli modules install')
   .arguments('[moduleNameWithVersion]', 'name of the module. Example: core. You can also pass version number: core@1.0.0')
   .action(async (moduleNameWithVersion) => {
     const spinner = ora({ text: "Modules install", stream: process.stdout, spinner: 'bouncingBar' }).start();
 
     try{
-      let localModules = readLocalModules();
+      let localModules = configFiles.readLocalModules();
       if(moduleNameWithVersion){
         const [moduleName, moduleVersion] = moduleNameWithVersion.split('@');
         localModules = await addNewModule(moduleName, moduleVersion, localModules, Portal.moduleVersions);
-        writePosModules(localModules);
+        configFiles.writePosModules(localModules);
         spinner.succeed(`Added module: ${moduleName}@${localModules[moduleName]}`);
       }
 
@@ -64,8 +45,8 @@ program
       } else {
         spinner.start('Resolving module dependencies');
         const modulesLocked = await resolveDependencies(localModules, Portal.moduleVersions);
-        writePosModulesLock(modulesLocked);
-        spinner.succeed(`Modules lock file generated: ${posModulesLockFilePath}`);
+        configFiles.writePosModulesLock(modulesLocked);
+        spinner.succeed(`Modules lock file generated: ${configFiles.posModulesLockFilePath}`);
       }
     } catch(e) {
       // throw e;
