fd2289da307cd7f369f3fbc4205208290a0c3531
piotrze <piotr.bliszczyk@gmail.com>
2024-02-05 18:58:57 +0100

    Add command for updating module

--- bin/pos-cli-modules-update.js
+++ bin/pos-cli-modules-update.js
@@ -9,32 +9,28 @@ const configFiles = require('../lib/modules/configFiles');
 const { findModuleVersion, resolveDependencies } = require('../lib/modules/dependencies')
 const Portal = require('../lib/portal');
 
-const addNewModule = async (moduleName, moduleVersion, localModules, getVersions) => {
+const updateModule = async (moduleName, moduleVersion, localModules, getVersions) => {
   const newModule = await findModuleVersion(moduleName, moduleVersion, getVersions);
-  let modules;
   if(newModule){
-    if (moduleVersion || !localModules[moduleName]) {
-      modules = {...localModules, ...newModule};
-    } else {
-      modules = {...localModules };
-    }
+    const modules = {...localModules, ...newModule};
     return modules;
   } else {
     throw new Error(`Can't find module ${moduleName} with version ${moduleVersion}`);
   }
 };
 
+
 program
-  .name('pos-cli modules install')
-  .arguments('[module-name]', 'name of the module. Example: core. You can also pass version number: core@1.0.0')
+  .name('pos-cli modules update')
+  .arguments('<module-name>', 'name of the module. Example: core. You can also pass version number: core@1.0.0')
   .action(async (moduleNameWithVersion) => {
-    const spinner = ora({ text: "Modules install", stream: process.stdout, spinner: 'bouncingBar' }).start();
+    const spinner = ora({ text: "Modules update", stream: process.stdout, spinner: 'bouncingBar' }).start();
 
     try{
       let localModules = configFiles.readLocalModules();
       if(moduleNameWithVersion){
         const [moduleName, moduleVersion] = moduleNameWithVersion.split('@');
-        localModules = await addNewModule(moduleName, moduleVersion, localModules, Portal.moduleVersions);
+        localModules = await updateModule(moduleName, moduleVersion, localModules, Portal.moduleVersions);
         configFiles.writePosModules(localModules);
         spinner.succeed(`Added module: ${moduleName}@${localModules[moduleName]}`);
       }
