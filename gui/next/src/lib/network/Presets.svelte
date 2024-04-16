ea78cc929e5ba0dee5d8c2c3e05a5f44f8120d78
zx <zx@secondgate.pl>
2024-04-16 11:34:21 +0200

    Saving presets in local storage

--- gui/next/src/lib/network/Presets.svelte
+++ gui/next/src/lib/network/Presets.svelte
@@ -14,8 +14,27 @@ import Icon from '$lib/ui/Icon.svelte';
 // container for the presets (dom node)
 let container;
 // input with name for new presets to save (dom node)
-let newPresetNameInput;
+let createPresetNameInput;
+// presets list containing 'url' and 'description' for each preset (array of objects)
+let presets = (localStorage.posNetworkLogsPresets && JSON.parse(localStorage.posNetworkLogsPresets)) || [{
+  url: "order_by=target_processing_time&order=DESC",
+  description: "Slowest requests"
+},
+{
+  url: "aggregate=http_request_path&order_by=avg_target_processing_time&order=DESC",
+  description: "Aggregated slowest requests"
+},
+{
+  url: "order_by=_timestamp&order=DESC&lb_status_codes=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,419,420,420,421,422,423,424,425,426,427,428,429,431,451",
+  description: "Responded with 4∗∗ error"
+},
+{
+  url: "order_by=_timestamp&order=DESC&lb_status_codes=500,501,502,503,504,505,506,507,508,510,511",
+  description: "Responded with 5∗∗ error"
+}];
 
+
+// event dispatcher
 const dispatch = createEventDispatcher();
 
 
@@ -48,7 +67,7 @@ function handleKayboardShortcuts(event){
     if(container.contains(document.activeElement)){
       event.preventDefault();
       if(document.activeElement?.matches('li:first-child a')){
-        newPresetNameInput.focus();
+        createPresetNameInput.focus();
       } else {
         document.activeElement?.parentElement?.previousElementSibling?.querySelector('a')?.focus();
       }
@@ -73,6 +92,38 @@ onNavigate(() => {
   dispatch('close');
 });
 
+
+// purpose:   saves current preset in local storage
+// arguments: form submit event (event)
+// ------------------------------------------------------------------------
+function createPreset(event){
+  let query = new URLSearchParams(document.location.search);
+  // we don't need the time filter stored as it should be universal
+  query.delete('start_time');
+
+  presets = [...presets, {
+    url: query.toString(),
+    description: createPresetNameInput.value
+  }];
+
+  localStorage.posNetworkLogsPresets = JSON.stringify(presets);
+}
+
+
+// purpose:   deletes preset from local storage
+// arguments: form submit event (event)
+// ------------------------------------------------------------------------
+function deletePreset(event){
+  const data = new FormData(event.target);
+  const index = data.get('id');
+
+  presets.splice(index, 1);
+
+  presets = presets;
+
+  localStorage.posNetworkLogsPresets = JSON.stringify(presets);
+}
+
 </script>
 
 
@@ -80,18 +131,18 @@ onNavigate(() => {
 <style>
 
 /* new preset form */
-form {
+.create {
   margin-block-end: .5em;
 
   display: flex;
 }
 
-form :focus-visible {
+.create :focus-visible {
   position: relative;
   z-index: 1;
 }
 
-input[type="text"] {
+.create input[type="text"] {
   width: 100%;
   padding: .2rem .5rem .3rem;
 
@@ -100,7 +151,7 @@ input[type="text"] {
   border-bottom-right-radius: 0;
 }
 
-button[type="submit"] {
+.create button {
   display: flex;
   align-items: center;
   padding-inline: .5rem;
@@ -138,12 +189,18 @@ a {
 }
 
 /* delete button */
+li form {
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+}
+
 li button {
   width: 15px;
   height: 15px;
   padding: 2px;
   display: flex;
-  flex-shrink: 0;
+  align-items: center;
 
   opacity: 0;
   border-radius: .5em;
@@ -165,6 +222,14 @@ li button {
     color: var(--color-interaction-hover);
   }
 
+/* empty message */
+.message {
+  margin-block-end: .25rem;
+  padding-inline: .5rem;
+
+  font-style: italic;
+}
+
 </style>
 
 
@@ -174,41 +239,30 @@ li button {
 
 
 <div bind:this={container}>
-  <form action="">
-    <input type="text" placeholder="Save current view" bind:this={newPresetNameInput}>
+  <form class="create" on:submit|preventDefault={createPreset}>
+    <input type="text" placeholder="Save current view" bind:this={createPresetNameInput}>
     <button type="submit">
       <span class="label">Save currently selected filters as new preset</span>
       <Icon icon="plus" />
     </button>
   </form>
   <ul>
-    <li>
-      <a href="/network?order_by=target_processing_time&order=DESC">Slowest requests</a>
-      <button>
-        <span class="label">Delete 'Slowest requests' preset</span>
-        <Icon icon="x" />
-      </button>
-    </li>
-    <li>
-      <a href="/network?aggregate=http_request_path&order_by=avg_target_processing_time&order=DESC">Aggregated slowest requests</a>
-      <button>
-        <span class="label">Delete 'Aggregated slowest requests' preset</span>
-        <Icon icon="x" />
-      </button>
-    </li>
-    <li>
-      <a href="/network?order_by=_timestamp&order=DESC&lb_status_codes=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,419,420,420,421,422,423,424,425,426,427,428,429,431,451">Responded with 4∗∗ error</a>
-      <button>
-        <span class="label">Delete 'Responded with 4** error' preset</span>
-        <Icon icon="x" />
-      </button>
-    </li>
-    <li>
-      <a href="/network?order_by=_timestamp&order=DESC&lb_status_codes=500,501,502,503,504,505,506,507,508,510,511">Responded with 5∗∗ error</a>
-      <button>
-        <span class="label">Delete 'Responded with 5** error' preset</span>
-        <Icon icon="x" />
-      </button>
-    </li>
+    {#each presets as preset, index}
+      <li>
+        <a href="/network?{preset.url}">{preset.description}</a>
+        <form on:submit|preventDefault={deletePreset}>
+          <input type="hidden" name="id" value={index}>
+          <button type="submit">
+            <span class="label">Delete '{preset.description}' preset</span>
+            <Icon icon="x" />
+          </button>
+        </form>
+      </li>
+    {/each}
   </ul>
+  {#if !presets.length}
+    <div class="message">
+      You can save your current filters selection to get back to them quickly in the future.
+    </div>
+  {/if}
 </div>
