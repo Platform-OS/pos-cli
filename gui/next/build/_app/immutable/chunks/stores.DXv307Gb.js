7f84bbd1b29ab1ce0cac9cab087e634f13276138
Łukasz Krysiewicz <zx@secondgate.pl>
2024-10-29 13:32:22 +0100

    Update gui deps (#660)
    * Updated GUI deps
    
    * Updated playwright container
    
    * Updated timeout for logsv2 tests
    
    * Testing if 6secs will be ok
    
    * Waiting 10 secs?!
    
    * 20 sec
    
    * Again
    
    * New container image
    
    * Updated playwright
    
    * Updated
    
    * svelte deps update
    
    * Skipping new log tests as they were disabled

--- gui/next/build/_app/immutable/chunks/stores.DXv307Gb.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./entry.Ca8ZVz6z.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ gui/next/build/_app/immutable/nodes/18.Dsy_Gmjs.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.CzbwTKXv.js";import{S as t,i as e}from"../chunks/index.BeBfCTAL.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
