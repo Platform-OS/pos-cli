7c65068b1105737cced50ecc1807b3085ef797d3
dariusz gorzeba <dgorzeba@gmail.com>
2025-01-07 22:23:37 +0100

    env-add-with-partner-portal-url

--- bin/pos-cli-env-add.js
+++ bin/pos-cli-env-add.js
@@ -1,83 +1,31 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+const ServerError = require('../lib//ServerError');
 const logger = require('../lib/logger');
-const validate = require('../lib/validators');
-const Portal = require('../lib/portal');
-const waitForStatus = require('../lib/data/waitForStatus');
-const { readPassword } = require('../lib/utils/password');
-const { storeEnvironment, deviceAuthorizationFlow } = require('../lib/environments');
-const ServerError = require('../lib/ServerError');
+const addEnv = require('../lib/env-add/main')
 
-const saveToken = (settings, token) => {
-  storeEnvironment(Object.assign(settings, { token: token }));
-  logger.Success(`Environment ${settings.url} as ${settings.environment} has been added successfuly.`);
-};
-
-const help = () => {
-  program.outputHelp();
-  process.exit(1);
-}
-
-const checkParams = params => {
-  // validate.existence({ argumentValue: params.email, argumentName: 'email', fail: help });
-  if (params.email) validate.email(params.email);
-
-  validate.existence({ argumentValue: program.args[0], argumentName: 'environment', fail: help });
-
-  validate.existence({ argumentValue: params.url, argumentName: 'URL', fail: help });
-  if (params.url.slice(-1) != '/') {
-    params.url = params.url + '/';
-  }
-  validate.url(params.url);
-};
-
-
-const login = async (email, password, url) => {
-  return Portal.login(email, password, url)
-    .then(response => {
-      if (response) return Promise.resolve(response[0].token);
-    })
-}
-
+program.showHelpAfterError();
 program
   .name('pos-cli env add')
-  .arguments('[environment]', 'name of environment. Example: staging')
+  .arguments('<environment>', 'name of environment. Example: staging')
   .option('--email <email>', 'Partner Portal account email. Example: admin@example.com')
-  .option('--url <url>', 'marketplace url. Example: https://example.com')
+  .requiredOption('--url <url>', 'marketplace url. Example: https://example.com')
+  .option('--partner-portal-url <partnerPortalUrl>', 'Partner Partner URL', 'https://partners.platformos.com')
   .option(
     '--token <token>',
     'if you have a token you can add it directly to pos-cli configuration without connecting to portal'
   )
   .action(async (environment, params) => {
     try {
-      checkParams(params);
-      const settings = { url: params.url, environment: environment, email: params.email };
-
-      if (params.token) {
-        token = params.token;
-      } else if (!params.email){
-        token = await deviceAuthorizationFlow(params.url);
-      } else {
-        logger.Info(
-          `Please make sure that you have a permission to deploy. \n You can verify it here: ${Portal.HOST}/me/permissions`,
-          { hideTimestamp: true }
-        );
-
-        const password = await readPassword();
-        logger.Info(`Asking ${Portal.HOST} for access token...`);
-
-        token = await login(params.email, password, params.url);
-      }
-
-      if (token) saveToken(settings, token);
+      await addEnv(environment, params);
     } catch (e) {
       if (ServerError.isNetworkError(e))
         ServerError.handler(e)
       else
-        logger.Error('Error');
-      process.exit(1);
+        logger.Error(e);
     }
+    process.exit(1);
   });
 
 program.parse(process.argv);
--- test/env-add.test.js
+++ test/env-add.test.js
@@ -1,32 +1,63 @@
 /* global jest */
 
+process.env['CI'] = 'true'
+
 const exec = require('./utils/exec');
 const cliPath = require('./utils/cliPath');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
-const run = (options) => exec(`${cliPath} env add ${options}`, { env: process.env });
-const readTokenForEnv = (envName) => {
-  const posFile = fs.readFileSync('.pos', 'utf8');
-  const posConfig = JSON.parse(posFile);
-  return posConfig[envName]['token'];
-}
+const fetchAuthData = require('../lib/settings').settingsFromDotPos;
 
-describe('env add', () => {
+const run = (options) => exec(`${cliPath} env add ${options}`);
+
+const addEnv = require('../lib/env-add/main')
+
+describe('commander env add', () => {
+  afterEach(() => exec(`rm .pos`));
+
   test('adding with email and token', async () => {
-    const { stdout, stderr } = await run('--url https://example.com --email pos-cli-ci@platformos.com --token 12345 env_url_email_token');
+    const { stdout, stderr } = await run('--url https://example.com --email pos-cli-ci@platformos.com --token 12345 e1');
 
-    expect(stdout).toMatch('Environment https://example.com/ as env_url_email_token has been added successfuly');
-    expect(readTokenForEnv('env_url_email_token')).toMatch('12345');
+    expect(stdout).toMatch('Environment https://example.com/ as e1 has been added successfuly');
+
+    const settings = fetchAuthData('e1')
+    expect(settings['token']).toMatch('12345');
   });
 
-  test.skip('adding with url', async () => {
-    // TODO: mock requests to portal
-    global.open = () => console.log('bar');
-    const { stdout, stderr } = await run('--url https:example.com env_url');
+  test('adding with email and token and partner_portal_url', async () => {
+    const { stdout, stderr } = await run('--url https://example.com --email pos-cli-ci@platformos.com --token 12345 e2 --partner-portal-url http://portal.example.com');
 
-    expect(stdout).toMatch('Environment https://example.com/ as env_url has been added successfuly');
-    expect(readTokenForEnv('env_url_email')).toMatch('12345');
+    expect(stdout).toMatch('Environment https://example.com/ as e2 has been added successfuly');
+
+    const settings = fetchAuthData('e2')
+    expect(settings['token']).toMatch('12345');
+    expect(settings['partner_portal_url']).toMatch('http://portal.example.com');
+  });
+});
+
+// const { apiRequest } = require('../lib/apiRequest');
+
+jest.mock('../lib/portal', () => ({
+  requestDeviceAuthorization: () => Promise.resolve({verification_uri_complete: "http://example.com/xxxx", device_code: "device_code"}),
+  fetchDeviceAccessToken: x => Promise.resolve({access_token: "12345"})
+}));
+
+describe('env add', () => {
+  afterEach(() => exec(`rm .pos`));
+
+  const environment = 'e1'
+  test('with --partner_portal_url', async () => {
+
+    const params = {
+      partnerPortalUrl: "http://portal.example.com",
+      url: "http://instance.example.com"
+    }
+
+    await addEnv(environment, params)
+    const settings = fetchAuthData(environment)
+    expect(settings['token']).toMatch('12345');
+    expect(settings['partner_portal_url']).toMatch('http://portal.example.com');
   });
 });
