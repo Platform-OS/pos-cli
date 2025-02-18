2d6783a2a25c86647249f8286b9096f11608aa38
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2025-02-18 14:48:42 +0100

    fix data.test.js

--- test/data.test.js
+++ test/data.test.js
@@ -21,7 +21,7 @@ describe('Data clean real', () => {
     const {code, stderr, stdout} = await exec(`echo "CLEAN DATA" | ${cliPath} data clean`, { env });
     expect(stderr).toMatch('WARNING!!! You are going to REMOVE your data from instance: http://example.com')
     expect(stderr).toMatch('There is no coming back.')
-    expect(stderr).toMatch('"statusCode": 405')
+    expect(stderr).toMatch('"statusCode": 403')
     expect(stderr).toMatch('"pathname": "/api/app_builder/data_clean"')
     expect(stderr).toMatch(env.MPKIT_URL)
   });
