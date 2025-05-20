dd7849d5e9dd3f65dca39f4cf53df5dfbcdcba59
Wojciech Grzeszczak <wojtekgrzeszczak@wp.eu>
2025-05-20 08:38:22 +0200

    Cleanup

--- gui/next/src/lib/parseValue.js
+++ gui/next/src/lib/parseValue.js
@@ -10,7 +10,6 @@ import { tryParseJSON } from '$lib/tryParseJSON.js';
 //				type of the value (string)
 // ------------------------------------------------------------------------
 const parseValue = (value, type) => {
-  debugger
   let parsed = {
     value: value,
     type: type
