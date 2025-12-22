1087b51842b012aa45360101bff0f0b782ada615
dariusz gorzeba <dgorzeba@gmail.com>
2025-12-22 14:15:59 +0100

    feat(fetch-logs): add pos-cli fetch-logs command
    and other ;)

--- test/unit/data.test.js
+++ test/unit/data.test.js
@@ -3,9 +3,9 @@ import cliPath from '#test/utils/cliPath';
 
 const env = Object.assign(process.env, {
   CI: true,
-  MPKIT_URL: 'http://example.com',
+  MPKIT_URL: 'http://google.com',
   MPKIT_TOKEN: '1234',
-  MPKIT_EMAIL: 'foo@example.com'
+  MPKIT_EMAIL: 'foo@google.com'
 });
 
 describe('Data clean', () => {
@@ -13,6 +13,17 @@ describe('Data clean', () => {
     const {code, stderr} = await exec(`echo "wrong confirm" | ${cliPath} data clean`, { env });
     expect(stderr).toMatch('Wrong confirmation. Closed without cleaning instance data.');
     expect(code).toEqual(1);
+  });
+});
+
+describe('Data clean real', () => {
+  test('shows message when wrong confirmation passed inline', async () => {
+    const {code, stderr, stdout} = await exec(`echo "CLEAN DATA" | ${cliPath} data clean`, { env });
+    expect(stderr).toMatch('WARNING!!! You are going to REMOVE your data from instance: http://google.com')
+    expect(stderr).toMatch('There is no coming back.')
+    expect(stderr).toMatch('"statusCode": 405')
+    expect(stderr).toMatch('"pathname": "/api/app_builder/data_clean"')
+    expect(stderr).toMatch(env.MPKIT_URL)
   });
 });
 
