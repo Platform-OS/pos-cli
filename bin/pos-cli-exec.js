fbf2569b2d72d9dca32997f6df0112998998db79
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2026-01-14 16:12:24 +0100

    add pos-cli exec graphql command

--- bin/pos-cli-exec.js
+++ bin/pos-cli-exec.js
@@ -5,4 +5,5 @@ const { program } = require('commander');
 program
   .name('pos-cli exec')
   .command('liquid <environment> <code>', 'execute liquid code on instance')
+  .command('graphql <environment> <graphql>', 'execute graphql query on instance')
   .parse(process.argv);
