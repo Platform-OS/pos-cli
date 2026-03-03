14d10160c95fccb61b2b4244a2f046018399f45c
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2026-03-03 13:11:58 +0100

    add pos-cli check update-docs (#700)

--- bin/pos-cli-check.js
+++ bin/pos-cli-check.js
@@ -5,4 +5,5 @@ program
   .name('pos-cli check')
   .command('run [path]', 'check Liquid code quality with platformos-check linter')
   .command('init [path]', 'initialize .platformos-check.yml configuration file')
+  .command('update-docs', 'download the latest platformOS Liquid documentation used by the linter')
   .parse(process.argv);
